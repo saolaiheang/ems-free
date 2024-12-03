17d5e0985b96b6a24a17b1282cf10584d8223ef0
saolaiheang <laiheang.sao@institute.pse.ngo>
2024-12-03 13:17:48 +0700

    create update delte Venue

--- routes/venueRouter.js
+++ routes/venueRouter.js
@@ -1,5 +1,10 @@
-import createVenue from '../controllers/venueController.js';
+import venueRouter from '../controllers/venueController.js';
 import express from 'express';
-const router= express.Router();
-router.post('/create-venue',createVenue);
-export default router;
+const {createVenue,getAllVenues,getVenueDetail,deleteVenue,updateVenue}=venueRouter
+const routerVenue= express.Router();
+routerVenue.post('/create-venue',createVenue);
+routerVenue.get('/getAllVenue',getAllVenues);
+routerVenue.get('/venueDetail/:id',getVenueDetail);
+routerVenue.delete('/deleteVenue/:id',deleteVenue);
+routerVenue.put('/update/:id',updateVenue);
+export default routerVenue;
