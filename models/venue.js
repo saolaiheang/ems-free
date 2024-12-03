b5e4c40c30968d963f9fa3b88ebc56da1b885738
saolaiheang <laiheang.sao@institute.pse.ngo>
2024-12-03 08:51:41 +0700

    create User model

--- models/venue.js
+++ models/venue.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 // const {Schema,model}=mongoose;
 const vanueSchema= new mongoose.Schema({
-    name:{type:[String],require:true},
-    location:{type:String,require:true},
-    capacity:{type:Number,require:true},
-    createBy:{type:String,require:true},
-    updateBy:{type:String,require:true},
+    name:{type:String,require:true,maxLength:20},
+    location:{type:String,require:true,max:500},
+    capacity:{type:Number,require:true,},
+    createBy:{ type: mongoose.Types.ObjectId, ref: "User", require: true },
+    updateBy:{ type: mongoose.Types.ObjectId, ref: "User", },
+    
 },
 {
     timestamps:true
 });
-
-const venues =Schema.model("venue",vanueSchema);
-export default venues;
+const Venues =mongoose.model("Venue",vanueSchema);
+export default Venues;
